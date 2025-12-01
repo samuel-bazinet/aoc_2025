1d3abe850897dddfd7908cf51e1820cc52ef84c2
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-01 00:38:05 -0500

    Done day 1

--- day1/part_1.py
+++ day1/part_1.py
@@ -1,3 +1,22 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    cur = 50
+    res = 0
+    for line in lines:
+        v = int(line[1:])
+        if line[0] == "L":
+            #print(f"left, {v}")
+            cur -= v
+            while cur < 0:
+                cur += 100
+        else:
+            #print(f"right, {v}")
+            cur += v
+            while cur > 99:
+                cur -= 100
+        if cur ==0:
+            res += 1
+
+    print(res)
+
--- day1/part_2.py
+++ day1/part_2.py
@@ -1,3 +1,34 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    cur = 50
+    res = 0
+    for line in lines:
+        started_at_0 = cur == 0
+        v = int(line[1:])
+        if line[0] == "L":
+            #print(f"left, {v}")
+            cur -= v
+            while cur < 0:
+                if not started_at_0:
+                    #print("passed 0")
+                    res += 1
+                cur += 100
+                started_at_0 = False
+            if cur == 0:
+                #print("at 0")
+                res += 1
+            
+        else:
+            #print(f"right, {v}")
+            cur += v
+            while cur > 99:
+                #print("passed 0")
+                res += 1
+                cur -= 100
+            if cur == 100:
+                cur = 0
+        #print(cur)
+
+    print(res)
+
