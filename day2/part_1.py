44785245e0130abe6ed7868c883c28b391c79c5c
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-02 00:11:48 -0500

    Done day 2

--- day2/part_1.py
+++ day2/part_1.py
@@ -1,3 +1,14 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    ranges = lines[0].split(',')
+    result = 0
+    for r in ranges:
+        r = r.split('-')
+        for i in range(int(r[0]), int(r[1])+1):
+            i = str(i)
+            l = len(i)
+            if l % 2 == 0:
+                if i[:l//2] == i[l//2:]:
+                    result += int(i)
+    print(result)
--- day2/part_2.py
+++ day2/part_2.py
@@ -1,3 +1,16 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    ranges = lines[0].split(',')
+    result = set()
+    for r in ranges:
+        r = r.split('-')
+        for i in range(int(r[0]), int(r[1])+1):
+            i = str(i)
+            l = len(i)
+            for j in range(1,l//2+1):
+                if l%j == 0:
+                    if i[:j]*(l//j) == i:
+                        result.add(int(i))
+    result = sum(result)
+    print(result)
