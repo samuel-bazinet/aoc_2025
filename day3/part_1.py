c6c70ed370b8ca8252409500d3c3eb86d09710f7
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-03 00:40:37 -0500

    Done day 3

--- day3/part_1.py
+++ day3/part_1.py
@@ -1,3 +1,17 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    result = 0
+    for line in lines:
+        line = line.strip()
+        combined = 0
+        for i, c in enumerate(line):
+            c = int(c)
+            for j, c2 in enumerate(line[i+1:]):
+                c2 = int(c2)
+                t = c*10 + c2
+                if t > combined:
+                    combined = t
+        result += combined
+    print(result)
+    
--- day3/part_2.py
+++ day3/part_2.py
@@ -1,3 +1,27 @@
 
+def recurse(line: str, d: int) -> int:
+    combined = 0
+    l = 0
+    r = -d+1
+    for _ in range(d):
+        max_i = 0
+        nl = 0
+        t_line = line[l:r] if r < 0 else line[l:]
+        for ind, j in enumerate(t_line):
+            if j > max_i:
+                max_i = j
+                nl = ind+l
+        combined = (combined * 10) + max_i
+        l = nl+1
+        r += 1
+    return combined
+
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    result = 0
+    for line in lines:
+        line = list(map(int, line.strip()))
+        combined = recurse(line, 12)
+        result += combined
+    print(result)
+    
