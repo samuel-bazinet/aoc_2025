a3a08727cf5f4b806aa57cda79a47712461e01e7
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-06 00:42:19 -0500

    Done day 6

--- day6/part_1.py
+++ day6/part_1.py
@@ -1,3 +1,27 @@
+from functools import reduce
+ops = {
+    "*",
+    "+"
+}
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    cols = []
+    first_line = True
+    for line in lines:
+        line = line.strip().split()
+        for i, num in enumerate(line):
+            if num in ops:
+                if num == "*":
+                    cols[i] = int(reduce(lambda x, y: x*y, cols[i], 1))
+                elif num == "+":
+                    cols[i] = sum(cols[i])
+                continue
+            if first_line:
+                cols.append([int(num)])
+            else:
+                cols[i].append(int(num))
+        if first_line:
+            first_line = False
+    print(sum(cols))
+    
--- day6/part_2.py
+++ day6/part_2.py
@@ -1,3 +1,37 @@
+from functools import reduce
+ops = {
+    "*",
+    "+"
+}
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    nums = []
+    result = 0
+    o = ""
+    for col in range(len(lines[0])):
+        b_c = 0
+        c_val = ""
+        for row in range(len(lines)):
+            try:
+                v = lines[row][col]               
+                if v in ops:
+                    o = v
+                    continue
+                if len(v.strip()) == 1:
+                    c_val += v
+                    b_c += 1
+            except:
+                pass
+        if b_c != 0:
+            nums.append(int(c_val))
+        if b_c == 0:
+            b_c = 0
+            if o == "*":
+                result += int(reduce(lambda x, y: x*y, nums, 1))
+            elif o == "+":
+                result += sum(nums)       
+            o = ""
+            nums = []
+            
+    print(result)
