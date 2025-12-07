11d77c5ae95f5bb3818e8f91f0369df7cfc5a457
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-07 00:28:44 -0500

    Done day 7

--- day7/part_1.py
+++ day7/part_1.py
@@ -1,3 +1,28 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    lines = [[c for c in line] for line in lines]
+    split_loc = []
+    s_loc = 0
+    for col, c in enumerate(lines[0]):
+            if c == 'S':
+                s_loc = col
+
+    hit = set()
+    active_c = set()
+    active_c.add(s_loc)
+    for r in range(len(lines)):
+        n_a = set()
+        for c in active_c:
+            if lines[r][c] == '^':
+                hit.add((r, c))
+                if c != 0:
+                    n_a.add(c-1)
+                if c!= len(lines[0])-1:
+                     n_a.add(c+1)
+            else:
+                 n_a.add(c)
+        active_c = n_a
+    print(len(hit))
+            
+
--- day7/part_2.py
+++ day7/part_2.py
@@ -1,3 +1,35 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    lines = [[c for c in line] for line in lines]
+    split_loc = []
+    s_loc = 0
+    for col, c in enumerate(lines[0]):
+            if c == 'S':
+                s_loc = col
+
+    active_c = {}
+    active_c[s_loc] = 1
+    for r in range(len(lines)):
+        n_a = {}
+        for c, v in active_c.items():
+            if lines[r][c] == '^':
+                if c != 0:
+                    if c-1 not in n_a:
+                        n_a[c-1] = v
+                    else:
+                         n_a[c-1] += v
+                if c!= len(lines[0])-1:
+                    if c+1 not in n_a:
+                        n_a[c+1] = v
+                    else:
+                         n_a[c+1] += v
+            else:
+                if c not in n_a:
+                    n_a[c] = v
+                else:
+                    n_a[c] += v
+        active_c = n_a
+    print(sum(active_c.values()))
+            
+
