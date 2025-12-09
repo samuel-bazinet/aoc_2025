5a2b7028ae41add58e6d667c820beec74e7c2d2a
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-12-09 01:22:10 -0500

    Done day 9

--- day9/part_1.py
+++ day9/part_1.py
@@ -1,3 +1,20 @@
 
+def dist(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return (abs(a[0]-b[0])+1) * (abs(a[1]-b[1])+1)
+
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+    coords = []
+    for line in lines:
+        line = line.strip().split(',')
+        coords.append((int(line[0]), int(line[1])))
+    maxi = 0
+    for coord in coords:
+        for c2 in coords:
+            if coord == c2:
+                continue
+            d = dist(coord, c2) 
+            if d > maxi:
+                maxi = d
+
+    print(maxi)
--- day9/part_2.py
+++ day9/part_2.py
@@ -1,3 +1,36 @@
+from shapely.geometry  import Point
+from shapely.geometry.polygon import Polygon
+
+def dist(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return (abs(a[0]-b[0])+1) * (abs(a[1]-b[1])+1)
 
 with open('input.txt', 'r') as file:
+
     lines = file.readlines()
+    coords: list[tuple[int, int]] = []
+    for line in lines:
+        line = line.strip().split(',')
+        x = int(line[0])
+        y = int(line[1])
+        coords.append((x, y))
+
+    poly = Polygon(coords)
+
+    maxi = 0
+    for s in coords:
+        for e in coords:
+            if s == e:
+                continue
+            s_x = s[0]
+            s_y = s[1]
+            e_x = e[0]
+            e_y = e[1]
+            c = (s_x, e_y)
+            p = (e_x, s_y)
+            rec = Polygon([s, c, e, p])
+            if poly.contains(rec):
+                d = dist(s, e)
+                if d > maxi:
+                    maxi = d
+
+    print(maxi)
